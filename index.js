df7dec2703a394bbce059df87a5196f040be95f6
Daniel Borges <msk@lywenn.eu.org>
2019-07-19 14:21:07 +0200

    added automatic reconnection to twitch and message to discord channel

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const Twitch = require("twitch").default;
+const Discord = require("discord.js");
 const ChatClient = require("twitch-chat-client").default;
 
-const { CLIENT_ID, CLIENT_SECRET, TOKEN, USERNAME, GAME_ID } = process.env;
+const { CODE, CLIENT_ID, CLIENT_SECRET, USERNAME, GAME, DISCORD, DISCORD_CHANNEL } = process.env;
 
 const UPDATE_DELAY = 60000; // milliseconds, 60000 = every minute
 const BETTER_RATIO = 1.5; // how much more many viewers a streamer must have to cut currently hosted streamer
@@ -12,11 +13,19 @@ const AutoGameHoster =
 	hostsLeft: 5,
 
 	currentHost: null,
+        knownStreams: [],
 
 	async update()
-	{		
+	{
+            // reconnect if necessary
+            if (this.token.isExpired)
+            {
+                this.token = await Twitch.refreshAccessToken(CLIENT_ID, CLIENT_SECRET, token.refreshToken);
+                await connect();
+            }
+
                 // check current host is still playing our target game
-		if (this.currentHost !== null && this.currentHost.gameId != GAME_ID)
+		if (this.currentHost !== null && this.currentHost.gameId != this.game.id)
 		{
 			this.chatClient.unhost(channel.name);
 			this.currentHost = null;
@@ -32,12 +41,13 @@ const AutoGameHoster =
 			return;
 		
                 // find the streamer with most views on target game
-		var streams = await this.client.helix.streams.getStreams({game: GAME_ID});
+		var streams = await this.client.helix.streams.getStreams({game: this.game.id});
 		streams = await streams.getAll();
 		if (streams !== null && streams.length > 0)
 		{
 			var bestStream = streams[0]; // list is ordered by viewers so first is best
-			if (bestStream !== this.currentHost && (this.currentHost === null || this.currentHost.viewers * BETTER_RATIO < bestStream.viewers))
+			if (bestStream !== this.currentHost
+                                && (this.currentHost === null || this.currentHost.viewers * BETTER_RATIO < bestStream.viewers))
 			{
 				var bestUser = await this.client.helix.users.getUserById(bestStream.userId);
 				var channel = await this.client.channels.getChannel(bestUser);
@@ -45,16 +55,25 @@ const AutoGameHoster =
 					.then(() => {
 						this.currentHost = bestStream;
 						this.hostsLeft--;
-						console.log("hosting: " + bestUser.displayName);
+                                                if (this.discordChannel != null)
+                                                    this.discordChannel.send(`We're now hosting ${bestUser.displayName}! ${channel.url}`);
 					})
 					.catch((e) => {
 						console.error("hosting " + bestUser.displayName + " failed: " + e)
 					});
 			}
+                        streams.forEach(stream => {
+                            if (stream != bestStream && !this.knownStreams.includes(stream))
+                            {
+                                this.discordChannel.send(`${bestUser.displayName} is streaming Pawarumi <3 ${channel.url}`);
+                                array_push(this.knownStreams, stream);
+                            }
+                        });
+                        this.knownStreams = this.knownStreams.filter(stream => streams.includes(stream));
 		}
                 else
                 {
-                    console.log("noone to host");
+                    this.knownStreams = [];
                 }
 	},
 
@@ -79,17 +98,30 @@ const AutoGameHoster =
 
 	async init()
 	{
-		// to get token, go to: https://id.twitch.tv/oauth2/authorize?client_secret=<secret>&client_id=<client>&redirect_uri=<oauthredirect>&response_type=token&scope=chat:edit+chat:read+channel_editor
-		this.client = Twitch.withCredentials(CLIENT_ID, TOKEN);
-		this.chatClient = await ChatClient.forTwitchClient(this.client);
-		this.chatClient.onHostsRemaining(this.onHostsRemaining);
-		this.chatClient.onUnhost(this.onUnhost);
-		await this.chatClient.connect();
-		await this.chatClient.waitForRegistration();
-		await this.chatClient.join(`#${USERNAME}`);
-		await this.update();
+	        this.token = await Twitch.getAccessToken(CLIENT_ID, CLIENT_SECRET, CODE, "http://localhost");
+                await this.connect();
+                this.discord = new Discord.Client();
+                var that = this;
+                this.discord.on("ready", () => { that.discordChannel = that.discord.channels.get(DISCORD_CHANNEL);});
+                this.discord.login(DISCORD);
 		setInterval(() => this.update().catch(console.error), UPDATE_DELAY);
 	},
+
+        async connect()
+        {
+                this.client = await Twitch.withCredentials(CLIENT_ID, this.token.accessToken);
+                this.game = await this.client.helix.games.getGameByName(GAME);
+                if (this.game != null)
+                {
+                    this.chatClient = await ChatClient.forTwitchClient(this.client);
+                    this.chatClient.onHostsRemaining(this.onHostsRemaining);
+                    this.chatClient.onUnhost(this.onUnhost);
+                    await this.chatClient.connect();
+                    await this.chatClient.waitForRegistration();
+                    await this.chatClient.join(`#${USERNAME}`);
+                    await this.update();
+                }
+        }
 };
 
 AutoGameHoster.init().catch(console.error);
